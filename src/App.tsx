10184524eb02e2e9c8e6eae27b9315918c0bb585
Medji <17dergachev@gmail.com>
2019-07-30 17:40:20 +0300

    Refactoring and adding Wrapped and TodoList components, App React.FC

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TodoListContainer from "./components/TodoListContainer";
+import Wrapped from "./components/Wrapped/Wrapped";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    render() {
+        return (
+            <Wrapped>
+                <TodoListContainer/>
+            </Wrapped>
+        );
+    }
 }
 
 export default App;
